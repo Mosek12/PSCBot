18cd87d80ae4348ac6e03df37a12624be6d5adc4
Mosek12 <79460068+Mosek12@users.noreply.github.com>
2021-05-12 12:34:11 +0200

    now it can vote

--- cogs/others.py
+++ cogs/others.py
@@ -1,5 +1,7 @@
 import discord
+from discord import colour
 from discord.ext import commands
+from discord.ext.commands.core import command
 
 
 class others(commands.Cog):
@@ -19,6 +21,27 @@ class others(commands.Cog):
                 await ctx.send(a)
         else:
             await ctx.send('Your argument must be less than 2137')
+
+    @commands.command()
+    # moderator, kierownik budowy, administrator, właściciel
+    @commands.has_any_role(769266991219212309, 769264017482973234, 799616680107180052, 572786262193340423)
+    async def vote(self, ctx, member: discord.Member, arg):
+
+        if arg == 'degrad':
+            string = 'zdegradowaniem'
+        elif arg == 'intemute':
+            string = 'natychmiastowym tymczasowym zmutowaniem'
+        else:
+            embed = discord.Embed(
+                description='Aby poprawnie użyć tej komendy, musisz zrobić to według wzoru `vote @użytkownik degrad/intemute`',
+                colour=discord.Colour.random()
+            )
+        embed = discord.Embed(
+            description=f'Głosowanie użytkownika {ctx.author.mention} za {string} {member.mention}',
+            colour=discord.Colour.random()
+        )
+        await ctx.send(embed=embed)
+        await ctx.add_reaction(ctx, ':white_check_mark:')
 
 
 def setup(bot):
